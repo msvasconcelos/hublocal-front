54eb4b762c0c4f3bae74680c8b925d945ce05fac
Matheus Vasconcelos <matheus-s.v@hotmail.com>
2020-07-27 13:56:17 -0300

    developed function of creating and style of the page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import { FiUser, FiInfo, FiSmile } from 'react-icons/fi';
 import api from '../../services/api';
+import logo from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-import { Container } from './styles';
+import { Container, TableContainer } from './styles';
 
 interface dataTask {
   id: string;
   name: string;
   description: string;
   status: 'not_done' | 'in_progress' | 'done';
+  // eslint-disable-next-line camelcase
+  created_at: Date;
+}
+
+interface createTask {
+  name: string;
+  description: string;
+  status: 'not_done' | 'in_progress' | 'done';
+  // eslint-disable-next-line camelcase
+  user_id: string;
 }
 
 const Dashboard: React.FC = () => {
@@ -19,6 +35,7 @@ const Dashboard: React.FC = () => {
   // return <h1>Página Dashboard2222</h1>;
 
   const [tasks, setTasks] = useState<dataTask[]>([]);
+  const formRef = useRef<FormHandles>(null);
 
   useEffect(() => {
     async function loadTask(): Promise<void> {
@@ -33,30 +50,88 @@ const Dashboard: React.FC = () => {
     loadTask();
   }, []);
 
+  const handleSubmit = useCallback(
+    async ({ name, status, user_id, description }: createTask) => {
+      const taskId = window.localStorage.getItem('@HubLocal:user');
+      if (taskId) {
+        const userId = JSON.parse(taskId);
+        const testeIdUser = userId.id;
+        await api.post('/task', {
+          description,
+          name,
+          user_id: testeIdUser,
+          status: 'not_done',
+        });
+        history.go(0);
+      }
+
+      // } catch (error) {
+      //   console.log(error);
+      //   throw new Error('erro ao cadastrar');
+      //   return;
+      // }
+    },
+    [],
+  );
+
   async function deleteTask(id: string): Promise<void> {
-    await api.delete(`/task/${id}`);
-    history.go(0);
+    try {
+      await api.delete(`/task/${id}`);
+      history.go(0);
+    } catch (error) {
+      throw new Error('erro ao deletar');
+    }
   }
 
   return (
     <>
       <Container>
-        <span>LISTA DE TASKS</span>
-        {tasks.map(task => (
-          <tr key={task.id}>
-            <td className="name">{task.name}</td>
-            <td className="name">{task.description}</td>
-            <td className="name">{task.status}</td>
-            <button
-              type="submit"
-              onClick={() => {
-                deleteTask(task.id);
-              }}
-            >
-              Deletar
-            </button>
-          </tr>
-        ))}
+        <div>
+          <img src={logo} alt="Todo List" />
+        </div>
+        {/* <a href="/">Voltar</a> */}
+        <Link to="/">Voltar</Link>
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Cadastrar novo Task</h1>
+
+          <Input name="name" icon={FiSmile} placeholder="Nome da tarefa" />
+          <Input name="description" icon={FiInfo} placeholder="Descricão" />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome da Tarefa</th>
+                <th>Descricão</th>
+                <th>Status</th>
+                <th>Data criação</th>
+                <th>#</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {tasks.map(task => (
+                <tr key={task.id}>
+                  <td className="title">{task.name}</td>
+                  <td className="">{task.description}</td>
+                  <td>{task.status}</td>
+                  <td>{task.created_at}</td>
+
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      deleteTask(task.id);
+                    }}
+                  >
+                    Deletar
+                  </button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
       </Container>
     </>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,11 +1,75 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 40px;
-  font-weight: bold;
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 40px 20px;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+    font-weight: bold;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  div {
+    text-align: center;
+  }
 `;
 
-export const Container = styled.div`
-  font-size: 40px;
-  font-weight: bold;
+export const Content = styled.div`
+  display: flex;
+  width: 100%;
+  place-content: center;
+  max-width: 700px;
+  align-items: center;
+`;
+
+export const TableContainer = styled.section`
+  margin-top: 24px;
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+    th {
+      color: #969cb3;
+      font-weight: normal;
+      padding: 20px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 24px;
+    }
+    td {
+      padding: 20px 32px;
+      border: 0;
+      background: #fff;
+      font-size: 16px;
+      font-weight: normal;
+      color: #969cb3;
+
+      &.title {
+        color: #363f5f;
+      }
+      button {
+        border: 0;
+      }
+      button img {
+        cursor: pointer;
+        border: 0;
+      }
+      svg {
+        width: 30px;
+        height: 30px;
+      }
+    }
+    td:first-child {
+      border-radius: 8px 0 0 8px;
+    }
+    td:last-child {
+      border-radius: 0 8px 8px 0;
+    }
+  }
 `;
